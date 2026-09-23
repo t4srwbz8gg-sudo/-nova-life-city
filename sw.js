5d42aa20d41782fa61d987e9a71858d52607b9d9
t4srwbz8gg-sudo <t4srwbz8gg@privaterelay.appleid.com>
2026-09-23 14:39:48 +0900

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='nova-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
